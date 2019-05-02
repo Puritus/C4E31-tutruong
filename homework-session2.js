69bd0a961f2dd926283838458528c33ba2405b38
Tu Truong <49758689+Puritus@users.noreply.github.com>
2019-05-02 12:57:20 +0700

    Add files via upload

---
+++ homework-session2.js
@@ -0,0 +1,89 @@
+// 1. 
+// Boolean is a logical statement that is either TRUE or FALSE
+// Examples: 10 == 11 (false); mindx (true); 834 (false);
+
+// 2.
+// a,
+// A flow chart is a graphical or symbolic representation of a process;
+// Each step in the process is represented by a different symbol and contains a short description of the process step;
+// The flow chart symbols are linked together with arrows showing the process flow direction;
+
+// b,
+// Insert name:
+
+// "Huy be" ~> "Hand some" ~> Finish the process.
+// "Huy big" ~> true ~> Finish the process.
+// Other name ~> Open: https://www.youtube.com/watch?v=04854XqcfCY
+
+// 3.
+// Nesting if/else statements helps to organize and isolate conditions in order to avoid testing the same condition twice or to minimize the number of times various tests need to be performed;
+// Examples:
+//  if(weight < 10){
+//      console.log("small");
+//  } else if(weight < 18){
+//      console.log("big");
+// };
+
+// 4.
+// a,
+
+// clear();
+// color(red);
+// for(i=1;i<=4;i++){
+// lt(35);
+// fd(50);
+// rt(70);
+// fd(50);
+// rt(110);
+// fd(50);
+// rt(70);
+// fd(50);
+// lt(125)};
+
+// b,
+
+// clear();
+// color("blue");
+// rt(30);
+// for(let i=0;i<17;i++){
+//   fd(100);
+//  if(i<2){
+//   rt(120);
+//   };
+//  else if(i<7){
+//    rt(72);
+//   }
+
+//   else if(i<11){
+//     color("red");
+//     rt(90);
+//   }
+//  else{
+//     rt(60);
+//   }
+// }
+
+// 5.
+// a,
+
+// let height = prompt("Import your height(cm):");
+// let weight = prompt("Import your weight(kg):");
+// let bmi = (Number(weight)/100) / (Number(height)^2);
+// if (bmi < 16){
+//   console.log("severely underweight");
+// } else if(16 <= bmi < 18.5){
+//   console.log("underweight");
+// } else if(18.5 <= bmi < 25){
+//   console.log("normal");
+// } else{
+//   console.log("obese");
+// };
+
+// b,
+
+// n = prompt("Import a Number");
+// u = 1
+// for (i = 1; i <= n; i++){
+//     u = u * i
+// }
+// console.log(u);
