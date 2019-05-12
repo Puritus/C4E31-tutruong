c694fd4ef998c80f98ef139b9056e03a8dccff3a
Tu Truong <49758689+Puritus@users.noreply.github.com>
2019-05-13 05:38:22 +0700

    Add files via upload

---
+++ homework-session5.js
@@ -0,0 +1,33 @@
+// 1.
+// let a = document.getElementById("demo-a");
+// let btn = document.getElementById("btn-demo");
+// let input = document.getElementById("demo-input");
+// btn.addEventListener("click", () => {
+//     input.value += a.href;  
+//     console.log(input.value);
+// });
+
+// 2.
+// let select = document.getElementById("sl");
+// console.dir(select);
+// select.addEventListener("click", () => {
+//     select.remove(select);
+// });
+
+// 3.
+// let table = document.getElementById("table");
+// let button = document.getElementById("button");
+
+
+
+// button.addEventListener("click", () => {
+//     console.dir(table);
+
+//     let rowNumber = prompt("Enter row number:");
+//     let rowsNumber = Number(rowNumber) - 1;
+//     let columnNumber = prompt("Enter column number:");
+//     let text = prompt("Enter text:");
+
+// chưa tìm ra cách giải quyết :<
+
+// }); 
